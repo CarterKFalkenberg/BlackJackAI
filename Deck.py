f27d32e559574b0ce82e2e20acca469cf96f59b4
CarterFalkenberg <ckfalk21@gmail.com>
2022-11-10 21:16:07 -0500

    added dealCard method

--- Deck.py
+++ Deck.py
@@ -3,12 +3,23 @@ class Deck:
 
     # create deck (made up of 6 decks)
     def __init__(self):
+        # keep track of which card was last dealt
+        self.current_card_index = -1
         # 24 aces, 2s, 3s, ..., 9s
         self.cards = ["A" for i in range(24)]
         self.cards += [int(i/24)+1 for i in range(24*8)]
         # 96 10s (24 10s, Js, Qs, Ks)
         self.cards += [10 for i in range(96)]
         random.shuffle(self.cards)
+    
+    # deal the next card and update self.current_card_index
+    def dealCard(self):
+        self.current_card_index += 1
+        return self.cards[self.current_card_index]
+
+
+    
+    
         
 
 
